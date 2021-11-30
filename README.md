3f32d90dc261f8f56344578e2ce91962f05bfecf
Manish Kumawat <BurningTiles>
2021-11-30 18:19:56 +0530

    Updated

--- README.md
+++ README.md
@@ -16,6 +16,11 @@ It is a Password Manager works completely offline with main focus on design whic
 # [Download Apk](https://github.com/BurningTiles/PassMan/releases/download/2.0/app-arm64-v8a-release.apk)
 # [Other Releases](https://github.com/BurningTiles/PassMan/releases/tag/2.0)
 
+### [CSV to JSON](csv_to_json)
+- To convert chrome passwords into json.
+- Copy the data from site and paste in text file and name it with extension '.json'.
+- Open PassMan settings and import the file.
+
 <br>
 <br>
 <img src="home.jpg" width="250px" /><img src="detail.jpg" width="250px"/><img src="settings.jpg" width="250px"/>
---
+++ csv_to_json/script.js
@@ -0,0 +1,46 @@
+async function loadFile(file) {
+	let pass = [];
+	let other = {};
+	let text = await file.text();
+	let lines = text.split("\n");
+	for(i in lines){
+		data = {}
+		if(lines[i].length<4) continue;
+		tmp = lines[i].split(",");
+		console.log(tmp);
+		let id = Math.random().toString(16).substr(2, 12);
+		data["id"] =  id; 
+		let time = Date.now();
+		let p = tmp[3];
+		if(p[p.length-1]=='\r') p = p.substr(0, p.length-1);
+		if(p.length<1) p = "tempPassword";
+		data["name"] = tmp[0].length>0 ? tmp[0] : "tempName";
+		data["uid"] = tmp[2].length>0 ? tmp[2] : "tempEmail";
+		data["password"] = p;
+		data["url"] = tmp[1];
+		data["remarks"] = "";
+		data["timestamp"] = time
+		other[id] = [{
+			"uid":tmp[2].length>0 ? tmp[2] : "tempEmail",
+			"password":p,
+			"timestamp":time
+		}]
+		pass.push(data);
+	}
+	console.log(pass);
+	console.log(other);
+	let jsonData = "";
+	for(i in pass){
+		jsonData += JSON.stringify(pass[i]) + ",\n"
+	}
+	document.getElementById("pass").innerHTML = jsonData.substr(0, jsonData.length-2);
+
+	jsonData = "";
+	for(key in other){
+		tmp = "\"" + key + "\":" + JSON.stringify(other[key]) + ",\n";
+		jsonData += tmp;
+	}
+	document.getElementById("other").innerHTML = jsonData.substr(0, jsonData.length-2);
+}
+
+// Math.random().toString(16).substr(2, 12)
